203270b07050be41221026ffaf9de1a20a3c06a9
gunwooknam2023 <taeme0912@naver.com>
2025-09-10 21:32:51 +0900

    fix: frontendUrls 변경 (단일 -> 배열)

--- stats-service/src/main/java/com/example/devnote/stats_service/config/WebConfig.java
+++ stats-service/src/main/java/com/example/devnote/stats_service/config/WebConfig.java
@@ -5,15 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Value("${app.frontend-url}")
-    private String frontendUrl;
+    private List<String> frontendUrls;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl)
+                .allowedOrigins(frontendUrls.toArray(new String[0]))
                 .allowedMethods("GET","POST","OPTIONS")
                 .allowCredentials(true);
     }

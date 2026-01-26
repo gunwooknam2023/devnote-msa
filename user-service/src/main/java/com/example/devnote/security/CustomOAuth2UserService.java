bc6f793e6330a06abf90f2bceb4011308ff9bf7c
gunwooknam2023 <taeme0912@naver.com>
2026-01-26 21:44:53 +0900

    chore: OAuth2 인증 과정 로그 추가

--- user-service/src/main/java/com/example/devnote/security/CustomOAuth2UserService.java
+++ user-service/src/main/java/com/example/devnote/security/CustomOAuth2UserService.java
@@ -1,6 +1,7 @@
 package com.example.devnote.security;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private final OAuth2UserProcessor userProcessor;
@@ -22,15 +24,24 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
      */
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        // 1. 부모 클래스의 loadUser를 호출하여 기본적인 OAuth2User 객체를 가져옴
-        OAuth2User oauth2User = super.loadUser(userRequest);
-        String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
-        // 2. 중앙 관리되는 userProcessor를 통해 사용자 정보를 처리 (가입/업데이트)
-        var user = userProcessor.processUser(registrationId, oauth2User);
+        log.info("OAuth2 loadUser 시작: registrationId={}", userRequest.getClientRegistration().getRegistrationId());
 
-        // 3. 처리된 User 엔티티와 원본 oauth2User 정보를 담은 PrincipalDetails 객체를 생성하여 반환
-        // 이 반환된 객체가 Spring Security의 SecurityContext에 저장됨
-        return new PrincipalDetails(user, oauth2User);
+        try {
+            // 1. 부모 클래스의 loadUser를 호출하여 기본적인 OAuth2User 객체를 가져옴
+            OAuth2User oauth2User = super.loadUser(userRequest);
+            String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+            // 2. 중앙 관리되는 userProcessor를 통해 사용자 정보를 처리 (가입/업데이트)
+            var user = userProcessor.processUser(registrationId, oauth2User);
+
+            // 3. 처리된 User 엔티티와 원본 oauth2User 정보를 담은 PrincipalDetails 객체를 생성하여 반환
+            // 이 반환된 객체가 Spring Security의 SecurityContext에 저장됨
+            return new PrincipalDetails(user, oauth2User);
+        } catch (Exception e) {
+            log.error("OAuth2 인증 과정 중 에러 발생", e);
+            throw e;
+        }
+
     }
 }
